2f58e29e14c317b5ccc541965425b840f4458b9e
naripok <fernando.canteruccio@gmail.com>
2021-09-26 15:42:14 -0300

    chore: Update Web3Container to functional react

--- client/lib/Web3Container.js
+++ client/lib/Web3Container.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import getWeb3 from './getWeb3'
-import getContract from './getContract'
-import contractDefinition from './contracts/SimpleStorage.json'
+import { useState, useEffect } from "react";
+import getWeb3 from "./getWeb3";
+import getContract from "./getContract";
+import contractDefinition from "./contracts/SimpleStorage.json";
 
-export default class Web3Container extends React.Component {
-  state = { web3: null, accounts: null, contract: null };
+export default ({ render, renderLoading }) => {
+  const [state, setState] = useState({
+    web3: null,
+    accounts: null,
+    contract: null,
+  });
 
-  async componentDidMount () {
-    try {
-      const web3 = await getWeb3()
-      const accounts = await web3.eth.getAccounts()
-      const contract = await getContract(web3, contractDefinition)
-      this.setState({ web3, accounts, contract })
-    } catch (error) {
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      )
-      console.log(error)
-    }
-  }
+  useEffect(() => {
+    const setup = async () => {
+      try {
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        const contract = await getContract(web3, contractDefinition);
+        setState({ web3, accounts, contract });
+      } catch (error) {
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.log(error);
+      }
+    };
+    setup();
+  }, [render, renderLoading]);
 
-  render () {
-    const { web3, accounts, contract } = this.state
-    return web3 && accounts
-      ? this.props.render({ web3, accounts, contract })
-      : this.props.renderLoading()
-  }
-}
+  return state.web3 && state.accounts ? render(state) : renderLoading();
+};
